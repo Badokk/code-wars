923bb37fad481f3491f3e1e77b269f2009dad9d4
Badokk <badokk@gmail.com>
2022-03-13 00:06:27 +0100

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,6 +5,8 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
+        // "f" and "l" are quick and easy, but very bad names. Variable name should signify what it is and what it's for, so f should be more like "Game window" and l "Game title"
+        // or whatever they're supposed to be
         Frame f = new Frame("Demo");
         f.setLayout(new FlowLayout());
         f.setSize(200, 200);
