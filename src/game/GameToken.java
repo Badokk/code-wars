fc113185072777ec4a982f2164f8b86678230ac5
Badokk <badokk@gmail.com>
2022-03-13 00:12:53 +0100

    Update GameToken.java

--- src/game/GameToken.java
+++ src/game/GameToken.java
@@ -1,11 +1,13 @@
 package game;
 
 public enum GameToken {
+    // This enum is never cast to int, no need to define int values
     EMPTY(0),
     PLAYER_ONE(1),
     PLAYER_TWO(2),
     PADDING(3);
 
+    // Not used, remove it
     public final int value;
 
     GameToken(int value) {
